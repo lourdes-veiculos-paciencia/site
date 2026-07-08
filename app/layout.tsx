718a47baf485121ff511f28aad1ec1e5b6359c30
Leonamnery <leonam_nery@yahoo.com.br>
2026-07-08 13:50:49 -0300

    atualizacao logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,82 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Lourdes Veiculos",
-  description: "Estoque de veiculos seminovos com procedencia e atendimento especializado.",
+  metadataBase: new URL("https://www.lourdesveiculos.com.br"),
+
+  title: {
+    default: "Lourdes Veículos",
+    template: "%s | Lourdes Veículos",
+  },
+
+  description:
+    "Estoque de veículos seminovos com procedência, qualidade e atendimento especializado.",
+
+  keywords: [
+    "Lourdes Veículos",
+    "Veículos",
+    "Carros",
+    "Seminovos",
+    "Usados",
+    "Rio de Janeiro",
+    "Campo Grande",
+    "Automóveis",
+  ],
+
+  authors: [
+    {
+      name: "Lourdes Veículos",
+    },
+  ],
+
+  creator: "Lourdes Veículos",
+
+  publisher: "Lourdes Veículos",
+
+  openGraph: {
+    title: "Lourdes Veículos",
+
+    description:
+      "Estoque de veículos seminovos com procedência, qualidade e atendimento especializado.",
+
+    url: "https://www.lourdesveiculos.com.br",
+
+    siteName: "Lourdes Veículos",
+
+    locale: "pt_BR",
+
+    type: "website",
+
+    images: [
+      {
+        url: "/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Lourdes Veículos",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+
+    title: "Lourdes Veículos",
+
+    description:
+      "Estoque de veículos seminovos com procedência e atendimento especializado.",
+
+    images: ["/og-image.jpg"],
+  },
+
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({
@@ -27,7 +101,9 @@ export default function RootLayout({
       lang="pt-BR"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+      </body>
     </html>
   );
 }
