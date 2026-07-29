e9417839c9e23b25d7f3e00ef69a1911adf5c5bf
lourdes-veiculos-paciencia <lourdes.veiculos.paciencia@gmail.com>
2026-07-29 10:24:28 -0300

    Update Logo.tsx

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -11,20 +11,35 @@ export default function Logo({
   size = "md",
 }: Props) {
   const sizes = {
-    sm: "h-8 md:h-10",
-    md: "h-10 md:h-14",
-    lg: "h-12 md:h-16",
+    sm: {
+      width: 130,
+      height: 44,
+      className: "h-7 w-auto sm:h-8 md:h-10",
+    },
+    md: {
+      width: 170,
+      height: 58,
+      className: "h-8 w-auto sm:h-10 md:h-14",
+    },
+    lg: {
+      width: 220,
+      height: 75,
+      className: "h-10 w-auto sm:h-12 md:h-16",
+    },
   };
 
   return (
-    <Link href="/" className="flex items-center">
+    <Link
+      href="/"
+      className="flex items-center"
+    >
       <Image
         src="/logo/logo.png"
         alt={CONFIG.empresa}
-        width={220}
-        height={75}
+        width={sizes[size].width}
+        height={sizes[size].height}
         priority
-        className={`${sizes[size]} w-auto object-contain`}
+        className={sizes[size].className}
       />
     </Link>
   );
